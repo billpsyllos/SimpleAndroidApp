3d2cb19fab5c18815c662d0a4f5486828fcaac54
billpsyllos <billpsyllos1@gmail.com>
2014-09-21 01:58:18 +0300

    add listener to button and create a random method to generate random text views

--- app/src/main/java/com/example/fun/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/fun/funfacts/FunFactsActivity.java
@@ -1,9 +1,15 @@
 package com.example.fun.funfacts;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,6 +18,34 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare our view variables and assign the views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.ShowFactbutton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // The button was clicked, so update he fact label with a new fact
+                String fact  = "";
+                // Randomly select a fact
+                Random randoGenerator = new Random(); // Construct a new Random number generrator
+                int randomNumber  = randoGenerator.nextInt(3);
+                //fact = randomNumber + "";
+
+                if (randomNumber == 0){
+                    fact = "Eisai malakas";
+                }else if (randomNumber == 1){
+                    fact = "papara";
+                }else {
+                    fact = "ante geia";
+                }
+
+                //update the label with our dynamic fact
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
 
