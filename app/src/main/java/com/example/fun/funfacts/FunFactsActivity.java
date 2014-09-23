60f9fdab8fe04b37e2fe4a2455a4428d4707171d
billpsyllos <billpsyllos1@gmail.com>
2014-09-23 14:53:36 +0300

    ..

--- app/src/main/java/com/example/fun/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/fun/funfacts/FunFactsActivity.java
@@ -24,7 +24,6 @@ public class FunFactsActivity extends Activity {
 
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
